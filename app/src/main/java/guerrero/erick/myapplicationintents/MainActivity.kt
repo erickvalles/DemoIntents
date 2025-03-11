26fde233cf746220ec32a341213664aed7c7286c
Erick Guerrero <erickg@valles.udg.mx>
2025-03-11 09:40:19 -0600

    Enable view binding and set it up in MainActivity
    This commit enables view binding in the project and specifically sets it up in `MainActivity.kt` to access views directly, replacing the need for `findViewById`.

--- app/src/main/java/guerrero/erick/myapplicationintents/MainActivity.kt
+++ app/src/main/java/guerrero/erick/myapplicationintents/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //establecer el binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
